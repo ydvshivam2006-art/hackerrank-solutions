38d4816c759864db8ab8520750f322b4dba758c6
shivam Yadav <ydvshivam2006@gmail.com>
2026-08-29 20:33:51 +0530

    ✅ Welcome to Java! [java] — approach1

---
+++ solutions/java/easy/welcome-to-java/solution-approach1.java
@@ -0,0 +1,21 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+// Problem     Welcome to Java!
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-29, 08:33 p.m.
+// ──────────────────────────────────────────────────
+
+
+public class Solution {
+    public static void main(String[] args){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+}
+        
+        
