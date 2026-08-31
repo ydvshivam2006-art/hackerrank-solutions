c51ee25f872b338c0601eb24075e0a426b54d9cc
shivam Yadav <ydvshivam2006@gmail.com>
2026-08-31 23:16:55 +0530

    ✅ Java Datatypes [java] — approach1

---
+++ solutions/java/easy/java-datatypes/solution-approach1.java
@@ -0,0 +1,42 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
+// Problem     Java Datatypes
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-31, 11:16 p.m.
+// ──────────────────────────────────────────────────
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) {
+                    System.out.println("* byte");
+                }
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) {
+                    System.out.println("* short");
+                }
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+                if (x >= Long.MIN_VALUE && x <= Long.MAX_VALUE) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+        sc.close();
+    }
+}
