1f9dfca4dddbf23928fe47be43e16cd7d746d90a
shivam Yadav <ydvshivam2006@gmail.com>
2026-09-10 20:36:54 +0530

    ✅ Valid Username Regular Expression [java] — approach1

---
+++ solutions/java/easy/valid-username-checker/solution-approach1.java
@@ -0,0 +1,16 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+// Problem     Valid Username Regular Expression
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-10, 08:36 p.m.
+// ──────────────────────────────────────────────────
+
+
+class UsernameValidator {
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
